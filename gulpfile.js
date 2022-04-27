8bfd33ced044afd1a8f0d1dc051f5608677bed1b
Yvan Tchuente <yvantchuente@gmail.com>
2022-04-27 20:58:49 +0100

    Update build config files: 'gulp.js' and 'package-lock.json'

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,47 @@
 /* Front-End Workflow Configuation File */
 
 // Initialize modules
-const { src, dest, watch, series, parallel } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const uglify = require("gulp-uglify");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 // const browsersync = require('browser-sync').create();
 
 // File path variables
 const sourceFiles = {
-    sassPath: "app/static/scss/**/*.scss",
-    jsPath: "app/static/js/**/*.js"
-}
+  sassPath: "app/static/src/scss/**/*.scss",
+  jsPath: "app/static/src/js/**/*.js",
+};
 
 const destination = {
-    cssPath: "app/static/dist/css",
-    jsPath: "app/static/dist/js"
-}
+  cssPath: "app/static/dist/css",
+  jsPath: "app/static/dist/js",
+};
 
 // Sass task
 function buildCSS() {
-    return src(sourceFiles.sassPath, { sourcemaps: true })
-        .pipe(sass())
-        .pipe(postcss([ autoprefixer(), cssnano() ]))
-        .pipe(dest(destination.cssPath, { sourcemaps: '.' }));
-}
-
-// Concatenates JS files into one JS Main file
-function JsConcatTask() {
-    return src(sourceFiles.jsPath, { sourcemaps: true })
-        .pipe(concat('main.js'))
-        .pipe(dest(destination.jsPath, { sourcemaps: "." }));
+  return src(sourceFiles.sassPath, { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest(destination.cssPath, { sourcemaps: "." }));
 }
 
 // Minifies the Main JS file for production environments
 function minifyJs() {
-    return src("app/static/dist/js/main.js", { sourcemaps: true })
-        .pipe(uglify())
-        .pipe(dest(destination.jsPath, { sourcemaps: "." }));
+  return src("app/static/dist/js/main.js", { sourcemaps: true })
+    .pipe(uglify())
+    .pipe(dest(destination.jsPath, { sourcemaps: "." }));
 }
-
-// // Browser synchronization task
-// function browsersyncServe(done) {
-//     browsersync.init({
-//         server: {
-//             baseDir: './app'
-//         }
-//     });
-//     done();
-// }
-
-// // Reload browser Task
-// function browsersyncReload(done) {
-//     browsersync.reload();
-//     done();
-// }
 
 // Watch task
 function watchTask() {
-    watch(sourceFiles.sassPath,series(buildCSS));
-    watch(sourceFiles.jsPath,series(JsConcatTask));
+  watch(sourceFiles.sassPath, series(buildCSS));
 }
 
 // Default Task
-exports.default = series(
-    buildCSS, 
-    JsConcatTask,
-    watchTask
-);
+exports.default = series(buildCSS, watchTask);
 
 // Minification of JS files
 exports.minifyJs = minifyJs;
